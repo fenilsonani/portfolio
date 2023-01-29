c9e5fe54bab144e61fef859311c9a932f986ce7c
Fenil Sonani <21bmiit160@gmail.com>
2023-01-29 16:31:28 +0530

    contact added

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 import { FaDownload } from 'react-icons/fa';
 
@@ -8,15 +9,15 @@ const Navbar = () => {
         <>
             <header className="text-gray-600 body-font">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    <Link href={"/home"} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                         <Image src="/logo.png" width={60} height={60} className='bg-black p-3 rounded-full' alt="tailblocks" />
                         <span className="ml-3 text-xl">Fenil Sonani</span>
-                    </a>
+                    </Link>
                     <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                        <a className="mr-5 hover:text-gray-900">First Link</a>
-                        <a className="mr-5 hover:text-gray-900">Second Link</a>
-                        <a className="mr-5 hover:text-gray-900">Third Link</a>
-                        <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+                        <Link href={"/home"} className="mr-5 hover:text-gray-900">Home</Link>
+                        <Link href={"/about"} className="mr-5 hover:text-gray-900">About</Link>
+                        <Link href={"/contact"} className="mr-5 hover:text-gray-900">Contact Me</Link>
+                        <Link href={""} className="mr-5 hover:text-gray-900">Fourth Link</Link>
                     </nav>
                     <button className="inline-flex items-center0 border-0 px-4 py-2 transition-all duration-150 ease-linear gap-2 focus:outline-none bg-gray-600 items-center hover:bg-gray-500 rounded text-white mt-4 md:mt-0"><FaDownload />Resume</button>
                 </div>
