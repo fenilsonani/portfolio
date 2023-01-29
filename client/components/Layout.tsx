38320c0a5130ee3b9ae4793c358b3a6243e3a727
Fenil Sonani <21bmiit160@gmail.com>
2023-01-29 17:12:44 +0530

    bug solve

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,14 +1,23 @@
+// @ts-nocheck
+
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
-const Layout = ({childern}) => {
+const Layout = ({ childern }) => {
+  // const { data, error } = useSWR('/api/navigation', fetcher)
+
+  // if (error) return <div>Failed to load</div>
+  // if (!data) return <div>Loading...</div>
+
   return (
-    <div>
-        <Navbar/>
+    <>
+      <Navbar />
+      <main>
         {childern}
-        <Footer/>
-    </div>
+      </main>
+      <Footer />
+    </>
   )
 }
 
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,14 @@
+// @ts-nocheck
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    // <Layout childern={<Component {...pageProps} />}>
     <Layout childern={<Component {...pageProps} />}>
-      <Component {...pageProps} />
+      {/* <Component {...pageProps} /> */}
     </Layout>
+
   )
 }
