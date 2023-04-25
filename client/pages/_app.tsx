ccc513271640a620bf6f7c828b3599ea7dc7d02b
Fenil Sonani <21bmiit160@gmail.com>
2023-04-25 23:56:05 +0530

    some change

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,13 +2,17 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
+  ;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // <Layout childern={<Component {...pageProps} />}>
     <Layout childern={<Component {...pageProps} />}>
       {/* <Component {...pageProps} /> */}
+      <Analytics />
     </Layout>
+
 
   )
 }
